ce79703c5140386c3ac328f808bb3f70d9990a3b
programmerNizam <mdnizam12109@gmail.com>
2024-01-15 02:29:36 +0600

    Update website logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         {/* <!-- Logo --> */}
         <a href="/">
-          <img className="h-[40px]" src="assets/lws-logo-en.svg" alt="Lws" />
+          <span className="text-white font-bold text-2xl uppercase">SmartGrade</span>
         </a>
         {/* Logo Ends  */}
         <a className="px-5 py-2 bg-[#172227] rounded-[44px]" href="#">
