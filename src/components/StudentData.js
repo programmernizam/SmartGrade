9a078883aa32414be56f38a236a377fc30fc2ea3
programmerNizam <mdnizam12109@gmail.com>
2024-01-14 17:29:44 +0600

    Make a student data component

---
+++ src/components/StudentData.js
@@ -0,0 +1,143 @@
+export const students = [
+    {
+        "id": 1,
+        "name": "John Doe",
+        "img": "https://i.pravatar.cc/150?img=2",
+        "score": "A",
+        "percentage": 75
+    },
+    {
+        "id": 2,
+        "name": "Jane Smith",
+        "img": "https://i.pravatar.cc/150?img=1",
+        "score": "A+",
+        "percentage": 82
+    },
+    {
+        "id": 3,
+        "name": "Bob Johnson",
+        "img": "https://i.pravatar.cc/150?img=3",
+        "score": "A-",
+        "percentage": 68
+    },
+    {
+        "id": 4,
+        "name": "Alice Williams",
+        "img": "https://i.pravatar.cc/150?img=4",
+        "score": "A",
+        "percentage": 77
+    },
+    {
+        "id": 5,
+        "name": "Chris Davis",
+        "img": "https://i.pravatar.cc/150?img=6",
+        "score": "A+",
+        "percentage": 90
+    },
+    {
+        "id": 6,
+        "name": "Emily Turner",
+        "img": "https://i.pravatar.cc/150?img=5",
+        "score": "A",
+        "percentage": 79
+    },
+    {
+        "id": 7,
+        "name": "Michael Harris",
+        "img": "https://i.pravatar.cc/150?img=7",
+        "score": "A+",
+        "percentage": 85
+    },
+    {
+        "id": 8,
+        "name": "Sara Martinez",
+        "img": "https://i.pravatar.cc/150?img=9",
+        "score": "A-",
+        "percentage": 66
+    },
+    {
+        "id": 9,
+        "name": "David Lee",
+        "img": "https://i.pravatar.cc/150?img=8",
+        "score": "A+",
+        "percentage": 91
+    },
+    {
+        "id": 10,
+        "name": "Olivia Brown",
+        "img": "https://i.pravatar.cc/150?img=10",
+        "score": "A",
+        "percentage": 74
+    },
+    {
+        "id": 11,
+        "name": "Brian Miller",
+        "img": "https://i.pravatar.cc/150?img=11",
+        "score": "A-",
+        "percentage": 63
+    },
+    {
+        "id": 12,
+        "name": "Jessica Taylor",
+        "img": "https://i.pravatar.cc/150?img=12",
+        "score":
+            "A",
+        "percentage": 72
+    },
+    {
+        "id": 13,
+        "name": "Mark Wilson",
+        "img": "https://i.pravatar.cc/150?img=13",
+        "score": "A+",
+        "percentage": 83
+    },
+    {
+        "id": 14,
+        "name": "Ella Moore",
+        "img": "https://i.pravatar.cc/150?img=15",
+        "score": "A",
+        "percentage": 78
+    },
+    {
+        "id": 15,
+        "name": "Andrew Jackson",
+        "img": "https://i.pravatar.cc/150?img=14",
+        "score": "A-",
+        "percentage": 69
+    },
+    {
+        "id": 16,
+        "name": "Grace White",
+        "img": "https://i.pravatar.cc/150?img=16",
+        "score": "A+",
+        "percentage": 86
+    },
+    {
+        "id": 17,
+        "name": "Daniel Garcia",
+        "img": "https://i.pravatar.cc/150?img=17",
+        "score": "A",
+        "percentage": 76
+    },
+    {
+        "id": 18,
+        "name": "Sophia Rodriguez",
+        "img": "https://i.pravatar.cc/150?img=19",
+        "score": "A-",
+        "percentage": 60
+    },
+    {
+        "id": 19,
+        "name": "Kevin Smith",
+        "img": "https://i.pravatar.cc/150?img=18",
+        "score": "A",
+        "percentage": 73
+    },
+    {
+        "id": 20,
+        "name": "Isabella Jones",
+        "img": "https://i.pravatar.cc/150?img=20",
+        "score": "A+",
+        "percentage": 94
+    }
+]
