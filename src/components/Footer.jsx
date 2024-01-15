dee921a6d78524b8014eb4c8ba5734af111fcb70
programmerNizam <mdnizam12109@gmail.com>
2024-01-15 13:57:07 +0600

    Update package.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <Footer>
-      <p>Copyright © 2024. All rights reserved ProgrammerNizam.</p>
+      <p className="text-center">Copyright © 2024. All rights reserved ProgrammerNizam.</p>
     </Footer>
   );
 }
