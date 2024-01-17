c95fe5ebe792aaac687d0ca3cd945b7b6dc0816f
programmerNizam <mdnizam12109@gmail.com>
2024-01-17 13:57:23 +0600

    Fix navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         {/* <!-- Logo --> */}
         <a href="/">
-          <span className="text-white font-bold text-2xl uppercase">SmartGrade   </span>
+          <span className="text-white font-bold text-2xl uppercase">SmartGrade  </span>
         </a>
         {/* Logo Ends  */}
         <a className="px-5 py-2 bg-[#172227] rounded-[44px]" href="#">
